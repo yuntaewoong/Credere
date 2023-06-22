dab63647d1eb8cdb9b1a4813ad3aee3ed80a8c8d
DESKTOP-R27HG89\Yoon <15ywt@naver.com>
2023-06-22 15:05:16 +0900

    Project Init

---
+++ Source/Credere/Credere.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Credere.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Credere, "Credere" );
---
+++ Source/Credere/CredereGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CredereGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CREDERE_API ACredereGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
