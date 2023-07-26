dd5d12c687a4a63803f1426d3dc20bd296903287
DESKTOP-R27HG89\Yoon <15ywt@naver.com>
2023-07-26 10:32:45 +0900

    Add Summoner Character Class

---
+++ Source/Credere/Public/Character/SummonerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/ABaseCharacter.h"
+#include "SummonerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CREDERE_API ASummonerCharacter : public ABaseCharacter
+{
+	GENERATED_BODY()
+	
+};
