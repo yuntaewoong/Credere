10206678de505b90d109c7b06fbe799d42ecdf84
DESKTOP-R27HG89\Yoon <15ywt@naver.com>
2023-07-28 10:37:19 +0900

    Set Warrior,Archer,Summoner AnimBP From Paragon Assets

--- Source/Credere/Private/Character/ASummonerCharacter.cpp
+++ Source/Credere/Private/Character/ASummonerCharacter.cpp
@@ -15,6 +15,12 @@ ASummonerCharacter::ASummonerCharacter()
 		UE_LOG(LogSkeletalMesh, Error, TEXT("Summoner Skeletal Mesh Not Loaded"));
 	GetMesh()->SetSkeletalMesh(skeletalMesh.Object);
 		
+	//Animation Blueprint로딩
+	static const ConstructorHelpers::FObjectFinder<UAnimBlueprint> animationBlueprint(TEXT("AnimBlueprint'/Game/Blueprints/AnimationBlueprints/SummonerAnimBP.SummonerAnimBP'"));
+	if (!animationBlueprint.Succeeded())
+		UE_LOG(LogSkeletalMesh, Error, TEXT("Summoner AnimBP Not Loaded"));
+	GetMesh()->SetAnimInstanceClass(animationBlueprint.Object->GeneratedClass);
+
 	GetMesh()->AddRelativeLocation(FVector(0.0, 0.0, SkeletalMeshZAdjust));
 	GetMesh()->AddRelativeRotation(FQuat(FRotator(0.0,SkeletalMeshYawAdjust,0.0)));
 }
