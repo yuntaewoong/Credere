33738abb53e34d28149ab6cda846c4b7487bfaa4
DESKTOP-R27HG89\Yoon <15ywt@naver.com>
2023-08-16 15:13:02 +0900

    Add UCredereAttributeSet Classes

---
+++ Source/Credere/AbilitySystem/Attributes/UCredereHealthAttributeSet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Attributes/UCredereAttributeSet.h"
+#include "UCredereHealthAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CREDERE_API UCredereHealthAttributeSet : public UCredereAttributeSet
+{
+	GENERATED_BODY()
+	
+};
